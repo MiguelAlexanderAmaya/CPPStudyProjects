97848b6805cb81e9d4ef4263025ca320caab74f4
Miguel Amaya <amayamiguel528@outlook.com>
2026-07-15 11:33:10 -0600

    STYLE: Change name of Project

---
+++ HForceTconnector/main.cpp
@@ -0,0 +1,29 @@
+/*
+ *  Calculate the horizontal force exerted on a T-connector pipe as shown below
+ *
+ *					    |	   |
+ *					    |	   |
+ *					----|	   |
+ *				--->		   |
+ *					----|      |
+ *						|	   |
+ *						|	   |
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float flow_rate;		// Mass flow rate;						Kg/s
+	float velocity;			// Velocity entering;					m/s
+	float horizontal_force;	// Horizontal forced exerted on pipe;	N
+    cout << "Write the flow rate(Kg/s): " << endl;
+	cin >> flow_rate;
+	cout << "Write the velocity(m/s):" << endl;
+	cin >> velocity;
+	horizontal_force = flow_rate * velocity;
+	cout << "The horizontal force is: " << endl
+		 << horizontal_force << "N" << endl;
+    return 0;
+}
